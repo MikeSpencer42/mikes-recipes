1c1189c38e7df50a0af2e522d545d70c15cc4c8a
Mike Spencer <chewierocks@gmail.com>
2019-11-01 15:13:44 -0400

    Adds Buefy component library. Adds support for SCSS. Creates first basic component. Adds a little style.

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,45 @@
+<template>
+    <header class="app-header has-text-white has-background-primary">
+        <!-- menu icon -->
+        <button type="button" 
+                class="icon-button" 
+                aria-label="Menu"
+                title="Menu">
+            <i class="fas fa-arrow-left"></i>
+        </button>
+
+        <h1 class="is-size-5 is-marginless has-text-centered is-uppercase">
+            Mike's Recipes
+        </h1>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.app-header {
+    display: flex;
+    align-items: center;
+    padding: .75rem;
+
+    h1 {
+        flex: 1;
+    }
+}
+
+/* TODO: this should be global? Or maybe a component */
+.icon-button {
+    font-size: 1.25rem;
+    padding: 0;
+    margin: 0;
+    background: transparent;
+    border: 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
+import Buefy from 'buefy'
+Vue.use(Buefy)
+
 import App from './App.vue'
+
+// Styles
+import './assets/css/fontawesome.all.min.css'
+import './sass/app.scss'
 
 Vue.config.productionTip = false
 
